16dc1e749ece23f1ab3efda9c90b1808b1f43b1c
Ragil Bimantara <ragil@itsavirus.com>
2022-08-13 08:56:54 +0200

    Initial Commit

---
+++ ExtensionSakti/ExtensionSakti.h
@@ -0,0 +1,18 @@
+//
+//  ExtensionSakti.h
+//  ExtensionSakti
+//
+//  Created by Ragil Bimantara on 13/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExtensionSakti.
+FOUNDATION_EXPORT double ExtensionSaktiVersionNumber;
+
+//! Project version string for ExtensionSakti.
+FOUNDATION_EXPORT const unsigned char ExtensionSaktiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionSakti/PublicHeader.h>
+
+
